cfacd0ce42a20cd6361279cc59768154f99b10d0
DESKTOP-TKCSRKO\willv <willvini45@gmail.com>
2026-07-17 13:15:54 +0100

    desenvolvimento do frame corpo

--- main.py
+++ main.py
@@ -35,6 +35,41 @@ imagem = Image.open('icon-speed.png')
 imagem = imagem.resize((55,55))
 imagem = ImageTk.PhotoImage(imagem)
 
+l_logo_imagem = Label(frame_logo, height=60, image=imagem, compound=LEFT, padx=10, anchor='nw', font=('Ivy 16 bold'), bg=co1, fg=co3)
+l_logo_imagem.place(x=20, y=0)
+l_logo_nome = Label(frame_logo, text='Speed Test', padx=10, anchor=NE , font=('Ivy 20 bold'), bg=co1, fg=co4)
+l_logo_nome.place(x=100, y=15)
+
+l_logo_linha = Label(frame_logo, width=350 , anchor=NW , font=('Ivy 1'), bg=co2)
+l_logo_linha.place(x=0, y=57)
+
+#configurando o frame_corpo
+
+l_download = Label(frame_corpo, text='65.7', anchor=NW , font=('arial 28'), bg=co1, fg=co4)
+l_download.place(x=44, y=25)
+l_download = Label(frame_corpo, text='Mbps download', anchor=NW , font=('Ivy 10'), bg=co1, fg=co4)
+l_download.place(x=30, y=70)
+
+imagem_down = Image.open('down.png')
+imagem_down = imagem_down.resize((50,50))
+imagem_down = ImageTk.PhotoImage(imagem_down)
+l_logo_imagem = Label(frame_corpo, height=60, image=imagem_down, compound=LEFT, padx=10, anchor='nw', font=('Ivy 16 bold'), bg=co1, fg=co3)
+l_logo_imagem.place(x=130, y=35)
+
+
+l_upload = Label(frame_corpo, text='65.7', anchor=NW , font=('arial 28'), bg=co1, fg=co4)
+l_upload.place(x=235, y=25)
+l_upload = Label(frame_corpo, text='Mbps upload', anchor=NW , font=('Ivy 10'), bg=co1, fg=co4)
+l_upload.place(x=230, y=70)
+
+imagem_up = Image.open('up.png')
+imagem_up = imagem_up.resize((50,50))
+imagem_up = ImageTk.PhotoImage(imagem_up)
+l_logo_imagem = Label(frame_corpo, height=60, image=imagem_up, compound=LEFT, padx=10, anchor='nw', font=('Ivy 16 bold'), bg=co1, fg=co3)
+l_logo_imagem.place(x=170, y=35)
+
+
+
 
 
 
